a296aeb43da1113124854eb397dcfb2663e8ec44
prasanth <prasanth@gmail.com>
2024-12-03 17:44:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Header from "./components/Header/header";
+
+const App = () => (
+  <>
+    <Header />
+  </>
+)
+export default App
---
+++ src/components/Header/header.js
@@ -0,0 +1,242 @@
+import './header.css'
+
+const Header = () => (
+    <>
+        <div className='app'>
+            <div className='fst-page'>
+                <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733140971/riskhawk-logo_1_v63lfu.png' className='logo'/>
+                <div className='options-container'>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733226075/file-search-02_zmydth.png' className='icon' />
+                            <p className='internal-text'>Internal Audit</p>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733141759/Vector_175_d8zpzt.png' className='vector'/>
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733225788/file-check-02_njzk9b.png' className='icon' />
+                            <p className='internal-text'>UAT Testing</p>
+                           
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733225799/link-03_qr0xz9.png' className='icon' />
+                            <p className='internal-text'>Intract</p>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733141759/Vector_175_d8zpzt.png' className='vector'/>
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733225288/users_dvifmv.png' className='icon' />
+                            <p className='internal-text'>Queue</p>
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733225388/sitemap_fb7p5i.png' className='icon' />
+                            <p className='internal-text'>Workflow</p>
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733225449/user_vbb3nc.png' className='icon' />
+                            <p className='internal-text'>Admin</p>
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733225458/box_sierxv.png' className='icon' />
+                            <p className='internal-text'>Asset</p>
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733225288/users_dvifmv.png' className='icon' />
+                            <p className='internal-text'>Parent</p>
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733225552/circle-off_rwqjta.png' className='icon' />
+                            <p className='internal-text'>Option One</p>
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733225622/circle-off_1_l80u8y.png' className='icon' />
+                            <p className='internal-text'>Option Two</p>
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733225634/wrench_kehw8v.png' className='icon' />
+                            <p className='internal-text'>All Accessoires</p>
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733141296/Icon_stjlft.png' className='icon' />
+                            <p className='internal-text'>Internal Audit</p>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733141759/Vector_175_d8zpzt.png' className='vector'/>
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733225668/star-06_swijrk.png' className='icon' />
+                            <p className='internal-text'>Decorating</p>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733141759/Vector_175_d8zpzt.png' className='vector'/>
+                        </div>
+                    </div>
+                    <div className='cards-container'>
+                        <div className='internal-audit'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733225764/gift_fi43ax.png' className='icon' />
+                            <p className='internal-text'>Presenting</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <nav className='navbar'>
+                <div>
+                    <p className='chat-text'>Chat / Feed chat </p>
+                </div>
+                <div className='nav-items'>
+  
+                        <div className='home'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733115852/home_suj9al.png' className='home-img'/>
+                            <p className='process-text'>Process Owner HOD</p>
+                        </div>
+                        <div className='admin-card'>
+                            <p className='admin-text'>Admin</p>
+                            <p className='palak-text'>Palak Bansal</p>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733134701/Ellipse_3855_ihkgdu.png' className='admin-img'/>                            
+                        </div>
+                        <div className='signout-card'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733134806/cog_biprcj.png' className='settings'/>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733134840/sign-out_xikmlt.png' className='signout'/>
+                            <p className='logout'>Logout</p>
+                    </div>
+                </div>
+            </nav>
+            <div className='second-page'>
+            <div className='messaging-card'> 
+                <div className='chat-heading'>
+                    <div className='chat-inner-container'>
+                        <p className='cb-text'>CB</p>
+                        <p className='name-text'>Cheyenne Bergson</p>
+                    </div>
+                </div>
+                <div className='chat-heading'>
+                    <div className='chat-inner-container'>
+                        <p className='cb-text'>CB</p>
+                        <p className='name-text'>Jonathan Higgins</p>
+                    </div>
+                </div>
+                <div className='chat-heading'>
+                    <div className='chat-inner-container'>
+                        <p className='cb-text'>CB</p>
+                        <p className='name-text'>Capt. Trunk</p>
+                    </div>
+                </div>
+                <div className='chat-heading'>
+                    <div className='chat-inner-container'>
+                        <p className='cb-text'>CB</p>
+                        <p className='name-text'>Hannibal Smi..</p>
+                    </div>
+                </div>
+                <div className='chat-heading'>
+                    <div className='chat-inner-container'>
+                        <p className='cb-text'>CB</p>
+                        <p className='name-text'>Capt. Trunk</p>
+                    </div>
+                </div>
+                <div className='chat-heading'>
+                    <div className='chat-inner-container'>
+                        <p className='cb-text'>CB</p>
+                        <p className='name-text'>Hannibal Smi..</p>
+                    </div>
+                </div>
+                <div className='msg-heading'>
+                    <div className='msg-inner-container'>
+                        <button className='comments'>
+                            <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733139401/comments_aazunc.png' 
+                            className='comments-img'
+                            /> Messaging
+                        </button>
+                    </div>
+                </div>
+                
+            </div>
+
+            <div className='containers-3'>
+                <div className='honible-smit-container'>
+                    <div className='main-card'>
+                        <p className='cb-card'> <span className='cb-text2'>CB</span></p>
+                        <div className='name-card'>
+                            <h1 className='hanibal-text'>Hannibal Smith</h1>
+                            <div className='location-address-card'>
+                                <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733219150/map-marker_gxhypg.png' className='map-img'/>
+                                <p className='address'>1 Market Street San Francisco, CA 94105</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='customer-details'>
+                        <div className='customer-id'>
+                            <p className='customer-text'>Customer ID</p>
+                            <div className='num-dot'>
+                               <p className='customer-id-num'>12345</p>
+                               <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733220956/Group_1686558931_nx7r1y.png' className='dots-img'/>
+                            </div>
+                        </div>
+                        <hr className='line'/>
+
+                        <div className='customer-email'>
+                            <p className='customer-text'>Email Address</p>
+                            <div className='num-dot'>
+                                <p className='customer-emailid'>rachel@sample.com</p>
+                                <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733220956/Group_1686558931_nx7r1y.png' className='dots-img'/>
+                            </div>
+                        </div>
+                        <div className='btn-phn-card'>
+                            <div className='phone-number-card'>
+                                <div className='customer-phone'>
+                                    <p className='phone-text'>Phone Number</p>
+                                    <div className='number-dot'>
+                                        <p className='phone-number'>8051298905</p>
+                                        <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733220956/Group_1686558931_nx7r1y.png' className='dots-img'/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='addbtn'>
+                                <img src="https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733223953/Vector_q5kpbu.png"  className='plus-img'/>
+                                <p className='add-text'>Add</p>
+                            </div>
+                        </div>
+                        <hr className='line'/>
+                    </div>  
+
+                    <div>
+                        <img src="https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733226940/Group_1686558930_khmnic.png" />
+                        <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733227007/Frame_1171286846_uhvile.png' />
+                        <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733227101/Frame_1171286847_s2ad9u.png'/>
+                        <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733227128/Frame_1686558944_fpjwq5.png' />
+                    </div>
+                </div>
+
+                <div className='img2'>
+                    <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733227206/Frame_1171287404_1_g5psu0.png' className='big-img'/>
+                </div>
+                    <div className='img3'>
+                        <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733227582/Frame_1171287414_fravym.png'/>
+                        <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733227572/Frame_1171287412_vlq9zg.png'/>
+                        <img src='https://res.cloudinary.com/dzpj3dnfw/image/upload/v1733227593/Frame_1171287413_vlqkyb.png'/>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+)
+
+export default Header
